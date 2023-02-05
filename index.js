681c22d1c36c3711cd9b18461de4fbbd34402fe3
Kristina_Klepik <kristygomel@gmail.com>
2023-02-05 19:24:56 +0300

    feat: add cors enable

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const accountRouter = require('./routes/account.routes');
 const boxRouter = require('./routes/box.routes');
 const userRouter = require('./routes/user.routes');
+const cors = require('cors')
 const PORT = process.env.PORT || 8080;
 const app = express();
-app.use(express.json());
+
+const corsOptions = {
+  origin: 'http://localhost:8080/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions),express.json());
 app.use('/', accountRouter);
 app.use('/', boxRouter);
 app.use('/account', userRouter);
