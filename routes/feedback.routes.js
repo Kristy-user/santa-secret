433e54b66d417a32bf8256e6e6224d30fa95b983
Kristina_Klepik <kristygomel@gmail.com>
2023-02-09 13:29:02 +0300

    feat: add box get method

--- routes/feedback.routes.js
+++ routes/feedback.routes.js
@@ -1,9 +1,10 @@
-const Router = require('express');
+const Router = require("express");
 const router = new Router();
-const feedbackController = require('../controller/feedback.controller');
-router.post('/feedback', feedbackController.createFeedback);
+const feedbackController = require("../controller/feedback.controller");
+router.post("/feedback", feedbackController.createFeedback);
 
-router.get('/feedback', feedbackController.getAllFeedbacks);router.get('/feedback/:id', feedbackController.getFeedbackByUser);
-router.put('/feedback', feedbackController.updateFeedback);
-router.delete('/feedback/:id', feedbackController.deleteFeedback);
+router.get("/feedback", feedbackController.getAllFeedbacks);
+router.get("/feedback/:id", feedbackController.getFeedbackByUser);
+router.put("/feedback", feedbackController.updateFeedback);
+router.delete("/feedback/:id", feedbackController.deleteFeedback);
 module.exports = router;
